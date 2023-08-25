3f7acec0a98b94807f030d14d3d52dcc6395ea94
medosobh <medo.sobh76@gmail.com>
2023-08-25 15:57:01 +0300

    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,11 @@
+# -*- coding: utf-8 -*-
 from . import clinic
+from . import appointment
+from . import doctor
+from . import insurance
+from . import insurance_policy
+from . import medicine
+from . import patient
+from . import pharmacist
+from . import staff
+from . import transaction
