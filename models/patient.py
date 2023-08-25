aeeb4b5d94b07f790a26cc1cfed7f466d1f4b223
medosobh <medo.sobh76@gmail.com>
2023-08-25 19:24:20 +0300

    update

--- models/patient.py
+++ models/patient.py
@@ -5,14 +5,46 @@ class Patient(models.Model):
     _name = 'ms_clinic.patient'
     _description = 'Patient'
 
-    name = fields.Char(string='Patient Name', required=True)
-    age = fields.Integer(string='Age')
-    gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('other', 'Other')], string='Gender')
-    address = fields.Text(string='Address')
-    phone = fields.Char(string='Phone')
-    email = fields.Char(string='Email')
-    medical_history = fields.Text(string='Medical History')
-
-    clinic_id = fields.Many2many('clinic', string='Clinic')
-    appointments = fields.One2many('ms_clinic.appointment', 'patient_id', string='Appointments')
-    insurance_id = fields.Many2one('ms_clinic.insurance', string='Patient Insurance')
+    name = fields.Char(
+        string='Patient Name',
+        required=True
+    )
+    identaty = fields.Char(
+        string='Patient Identaty',
+        required=True
+    )
+    age = fields.Integer(
+        string='Age'
+    )
+    gender = fields.Selection(
+        [
+            ('male', 'Male'),
+            ('female', 'Female'),
+        ],
+        string='Gender'
+    )
+    address = fields.Text(
+        string='Address'
+    )
+    phone = fields.Char(
+        string='Phone'
+    )
+    email = fields.Char(
+        string='Email'
+    )
+    medical_history = fields.Text(
+        string='Medical History'
+    )
+    clinic_id = fields.Many2many(
+        'ms_clinic.clinic',
+        string='Clinic'
+    )
+    appointments = fields.One2many(
+        'ms_clinic.appointment',
+        'patient_id',
+        string='Appointments'
+    )
+    insurance_id = fields.Many2one(
+        'ms_clinic.insurance',
+        string='Patient Insurance'
+    )
