7a5708f013cbf470bbbeaea7d6a14e66271561a6
medosobh <medo.sobh76@gmail.com>
2023-08-29 21:19:19 +0300

    update

--- models/appointments.py
+++ models/appointments.py
@@ -6,15 +6,24 @@ from datetime import date, datetime, timedelta
 class Appointments(models.Model):
     _name = "hospital.appointments"
     _description = "Information about appointments"
-    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _inherit = ["mail.thread", "mail.activity.mixin"]
 
-    name = fields.Char(string="Name", required=True)
-    clinic_id = fields.Many2one("clinic", string="Location")
-    patient_id = fields.Many2one("hospital.patient", string="Patient")
-    doctor_id = fields.Many2one("hospital.doctor", string="Doctor")
-    appointment_date = fields.Date(
-        string="Appointment Date", default=fields.Date.today()
+    state = fields.Selection(
+        [
+            ("draft", "Draft"),
+            ("confirm", "Confirm"),
+            ("inspection", "Inspection"),
+            ("reschedual", "Reschedual"),
+            ("closed", "Closed"),
+        ],
+        string="State",
+        default="draft",
     )
+    name = fields.Char(string="Name", required=True)
+    patient_id = fields.Many2one("hospital.patient", string="Patient")
+    clinic_id = fields.Many2one("clinic", string="Location")
+    doctor_id = fields.Many2one("hospital.doctor", string="Doctor")
+
     start_date = fields.Datetime(
         string="Start Date", required=True, tracking=True, default=datetime.now()
     )
@@ -31,10 +40,46 @@ class Appointments(models.Model):
     payment_amount = fields.Float(string="Payment Amount")
     payment_date = fields.Date(string="Payment Date")
     analytic_account_id = fields.Reference(
-        selection=[
-            ('account.analytic.account', 'Analytic Account')
-        ],
-        string='Analytic Account')
+        selection=[("account.analytic.account", "Analytic Account")],
+        string="Analytic Account",
+    )
+    next_date = fields.Date(string="Appointment Date",
+                            default=fields.Date.today())
+    parent_id = fields.Many2one(
+        "account.group", index=True, ondelete="cascade", readonly=True
+    )
+    parent_path = fields.Char(index=True)
+    child_id = fields.One2many(
+        comodel_name="farm.locations", inverse_name="parent_id", string="Child location"
+    )
+    company_id = fields.Many2one(
+        comodel_name="res.company",
+        string="Company",
+        change_default=True,
+        default=lambda self: self.env.company,
+        required=False,
+        readonly=True,
+    )
+    currency_id = fields.Many2one(
+        comodel_name="res.currency",
+        string="Currency",
+        related="company_id.currency_id",
+        readonly=True,
+        ondelete="set null",
+        help="Used to display the currency when tracking monetary values",
+    )
+    customer_invoice_count = fields.Integer(
+        string="Patient Invoice Count", compute="_compute_customer_invoice_count"
+    )
+    customer_invoice_total = fields.Integer(
+        string="Patient Invoice Total", compute="_compute_customer_invoice_total"
+    )
+
+    def reschedual_appointment(self):
+        # create new appointment
+        # set close state next date to old appointment rec
+
+        return
 
     @api.depends("start_date")
     def _get_end_date(self):
@@ -123,89 +168,101 @@ class Appointments(models.Model):
             "target": "current",
         }
 
+    def _compute_customer_invoice_count(self):
+        for rec in self:
+            customer_invoice_count = self.env["account.move"].search_count(
+                [("invoice_origin", "=", rec.name)]
+            )
+            rec.customer_invoice_count = customer_invoice_count
+
+    def _compute_customer_invoice_total(self):
+        for rec in self:
+            total_debit = sum(
+                self.env["account.move.line"]
+                .search([("sales_id", "=", rec.id)])
+                .mapped("debit")
+            )
+            total_credit = sum(
+                self.env["account.move.line"]
+                .search([("sales_id", "=", rec.id)])
+                .mapped("credit")
+            )
+            rec.customer_invoice_total = total_debit + total_credit
+        return rec.customer_invoice_total
+
 
 class diagnosis(models.Model):
-    _name = 'diagnosis'
-    _description = 'diagnosis'
-    _rec_name = 'name'
-    _order = 'name ASC'
+    _name = "diagnosis"
+    _description = "diagnosis"
+    _rec_name = "name"
+    _order = "name ASC"
 
     def _get_report_base_filename(self):
         return self.name
 
-    sequence = fields.Integer(
-        string='Sequence',
-        default=10)
+    sequence = fields.Integer(string="Sequence", default=10)
     description = fields.Text(
-        string='Diagnosis Description',
-        required=True,
-        copy=False
-    )
+        string="Diagnosis Description", required=True, copy=False)
     attachment = fields.Binary(
         string="Attachment File",
-        help='Attachment, one file to upload',
+        help="Attachment, one file to upload",
         required=False,
         tracking=True,
     )
     attachment_name = fields.Char(
-        string='Attachment Filename',
-        required=False,
-        tracking=True
+        string="Attachment Filename", required=False, tracking=True
     )
 
 
 class prescription(models.Model):
-    _name = 'prescription'
-    _description = 'prescription'
-    _rec_name = 'name'
-    _order = 'name ASC'
+    _name = "prescription"
+    _description = "prescription"
+    _rec_name = "name"
+    _order = "name ASC"
 
     def _get_report_base_filename(self):
         return self.name
 
-    sequence = fields.Integer(
-        string='Sequence',
-        default=10)
+    sequence = fields.Integer(string="Sequence", default=10)
     product_id = fields.Many2one(
-        comodel_name='product.product',
+        comodel_name="product.product",
         required=True,
-        domain="[('categ_id', '=', categ_id)]")
-    name = fields.Text(
-        string='Description',
-        required=False)
+        domain="[('categ_id', '=', categ_id)]",
+    )
+    name = fields.Text(string="Description", required=False)
     categ_id = fields.Many2one(
-        related='materials_id.category_id',
-        string='Category')
-    price_unit = fields.Float(
-        string='Price')
+        related="materials_id.category_id", string="Category")
+    price_unit = fields.Float(string="Price")
     product_uom = fields.Many2one(
-        comodel_name='uom.uom',
-        string='Unit of Measure',
-        related='product_id.uom_id',
-        domain="[('category_id', '=', product_uom_category_id)]")
-    qty = fields.Float(
-        'Quantity')
+        comodel_name="uom.uom",
+        string="Unit of Measure",
+        related="product_id.uom_id",
+        domain="[('category_id', '=', product_uom_category_id)]",
+    )
+    qty = fields.Float("Quantity")
     company_id = fields.Many2one(
-        comodel_name='res.company',
-        string='Company',
-        related='materials_id.company_id',
+        comodel_name="res.company",
+        string="Company",
+        related="materials_id.company_id",
         change_default=True,
         default=lambda self: self.env.company,
         required=False,
-        readonly=True)
-    currency_id = fields.Many2one(
-        comodel_name='res.currency',
-        string='Currency',
-        related='materials_id.currency_id',
         readonly=True,
-        help="Used to display the currency when tracking monetary values")
-    note = fields.Char(
-        string='Short Note')
+    )
+    currency_id = fields.Many2one(
+        comodel_name="res.currency",
+        string="Currency",
+        related="materials_id.currency_id",
+        readonly=True,
+        help="Used to display the currency when tracking monetary values",
+    )
+    note = fields.Char(string="Short Note")
     price_subtotal = fields.Monetary(
-        string='Subtotal',
-        compute='_compute_subtotal',
-        currency_field='currency_id',
-        store=True)
+        string="Subtotal",
+        compute="_compute_subtotal",
+        currency_field="currency_id",
+        store=True,
+    )
     appointments_id = fields.Many2one(
-        comodel_name='hospital.appointments',
-        string='Appointment')
+        comodel_name="hospital.appointments", string="Appointment"
+    )
