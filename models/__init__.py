b5aa2dbdab06c5412e452201eb7bf46c53c1617f
medosobh <medo.sobh76@gmail.com>
2023-09-08 13:49:04 +0300

    update

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,6 @@ from . import clinics
 from . import patients
 from . import staff
 from . import inherit
-from . import tickets
+from . import clinic_tickets
 from . import services
 from . import sales
